23b6fe15d80afe43802c730e3b19135639c03c6c
Dayron-z <zbastian.zapata@gmail.com>
2024-08-05 13:59:37 -0500

    cambios y uso de las apis

---
+++ src/models/Ipost.ts
@@ -0,0 +1,9 @@
+export interface Ipost {
+      id: number;
+      title: string;
+      description: string;
+      body: string;
+      multimediaUrl: string;
+      date: string;
+  creationDate: string;
+    }
